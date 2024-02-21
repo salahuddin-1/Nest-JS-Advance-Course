085da3a1f75aea9f76674d50a853de8285b088c8
Salahuddin Shaikh <salahuddinshaikh16@gmail.com>
2024-02-21 11:01:36 +0530

    Validation for config file

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -3,12 +3,17 @@ import {
   ConfigService,
   ConfigModule as NestConfigModule,
 } from '@nestjs/config';
+import * as Joi from 'joi';
 
 // This config module is gonna be a wrapper around the nestjs-config module
 @Module({
   imports: [
     // In this forRoot method, we'll load the environment variables from the .env file
-    NestConfigModule.forRoot(),
+    NestConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+    }),
   ],
   providers: [ConfigService],
   exports: [ConfigService],
