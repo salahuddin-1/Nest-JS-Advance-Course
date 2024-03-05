96f880e01ce84f78732fea9f2d356dffa2b6a1e9
Salahuddin Shaikh <salahuddinshaikh16@gmail.com>
2024-03-05 21:10:41 +0530

    ValidationPipe added in auth module

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(3000);
+
+  // Enable validation pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // If from the client side, the client sends a field that is not in the DTO,
+      // it will automatically strip it out
+      whitelist: true,
+    }),
+  );
+
+  // Enable Pinologger
+  app.useLogger(app.get(Logger));
+
+  await app.listen(3001);
 }
 bootstrap();
